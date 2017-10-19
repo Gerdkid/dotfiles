2478873783464ef277b9bb2ec667b07651c4b8bb
Gerdkid <joshgerding@gmail.com>
2017-10-18 20:51:25 -0400

    Setting up a controled way to handle dotfiles between systems

---
+++ mac/bash/aliases.sh
@@ -0,0 +1,21 @@
+#COMMAND ALIASES --------------------------------
+	alias ll='ls -al'
+	alias ls='ls -a'
+	alias projects='cd ~/projects'
+	alias vim='mvim -v'
+	alias dcc='docker rm $(docker ps -a -q)'
+	alias dci='docker rmi $(docker images -q)'
+	alias dca='dcc; dci'
+
+#NPM ALIASES ------------------------------------
+
+#GIT ALIASES ------------------------------------
+	alias gs='git status'
+
+#I CAN'T TYPE ALIASES ---------------------------
+	alias nom='npm'
+	alias gti='git'
+#TODO.TxT --------------------------------------
+alias t='/usr/local/bin/todo.sh -c -d /Users/joshgerding/dotfiles/utilities/todoTxt/config'
+
+#PROJECT CONFIGURATION ALIASES ------------------
---
+++ mac/bash/environment.sh
@@ -0,0 +1,13 @@
+#ENV VARIABLES ----------------------------------
+	#enable cli colors
+	export CLICOLOR=1
+	export LSCOLORS=GxFxCxDxBxegedabagaced
+
+        export ANDROID_HOME=/Users/joshgerding/Library/Android/sdk
+        export GOPATH=$HOME/Application_Development/go
+
+        export TODOTXT_DEFAULT_ACTION=ls
+
+        #nvm through brew
+        export NVM_DIR=~/.nvm
+        source $(brew --prefix nvm)/nvm.sh
